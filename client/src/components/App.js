0163c446f7fcaadc94f7b5c5b0e1a4530c073d8f
ahamium <ahamium@naver.com>
2019-02-23 01:50:29 +0900

    update poongcoin

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,12 +19,12 @@ class App extends Component {
         <img className='logo' src={logo}></img>
         <br />
         <div>
-          Welcome to the blockchain...
+         The blockchain of PoongCoin
         </div>
         <br />
         <div><Link to='/blocks'>Blocks</Link></div>
-        <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
-        <div><Link to='/transaction-pool'>Transaction Pool</Link></div>
+        <div><Link to='/conduct-transaction'>Conduct a Transaction(Sending PoongCoin)</Link></div>
+        <div><Link to='/transaction-pool'>Transaction Pool(Mining PoongCoin)</Link></div>
         <br />
         <div className='WalletInfo'>
           <div>Address: {address}</div>
